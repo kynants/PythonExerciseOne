94e27fd8fb494d69a45ca7bb197212b8203290d0
Kynan Stevenson <kynants@gmail.com>
2020-09-30 07:48:18 -0400

    Initial commit

---
+++ program.py
@@ -0,0 +1,9 @@
+import textwrap
+# textwrap.indent()
+
+print("Twinkle, twinkle, little star,\n"
+      "        How I wonder what you are!\n"
+      "                Up above the world so high,\n"
+      "                Like a diamond in the sky.\n"
+      "Twinkle, twinkle, little star,\n"
+      "        How I wonder what you are!")
